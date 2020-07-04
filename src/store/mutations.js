01725552288169501d1da8befd0dd2f42f466bd1
bfclouds <834298507@qq.com>
2020-07-04 14:36:08 +0800

    add store about player

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,7 +3,26 @@ import * as mutationsTypes from './mutationsTypes'
 const mutations = {
   [mutationsTypes.SET_SINGER](state, singer) {
     state.singer = singer
+  },
+  [mutationsTypes.SET_PLAYING_STATE](state, flag) {
+    state.playing = flag
+  },
+  [mutationsTypes.SET_FULL_SCREEN](state, flag) {
+    state.fullScreen = flag
+  },
+  [mutationsTypes.SET_PLAY_LIST](state, list) {
+    state.playList = list
+  },
+  [mutationsTypes.SET_SEQUENCE_LIST](state, list) {
+    state.sequenceList = list
+  },
+  [mutationsTypes.SET_MODE](state, mode) {
+    state.mode = mode
+  },
+  [mutationsTypes.SET_CURRENT_INDEX](state, index) {
+    state.currentIndex = index
   }
+
 }
 
 export default mutations
